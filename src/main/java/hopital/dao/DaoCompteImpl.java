3e2b1ba971b2353dc6ddaafd5a48b6a8a92f07bd
Coralinechrd <coraline.cochard@gmail.com>
2023-03-07 14:24:23 +0100

    dao

--- src/main/java/hopital/dao/DaoCompteImpl.java
+++ src/main/java/hopital/dao/DaoCompteImpl.java
@@ -1,39 +1,111 @@
 package hopital.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import hopital.model.Compte;
+import hopital.model.Patient;
 
 public class DaoCompteImpl implements DaoCompte{
+	
+	private Compte getCompte(ResultSet rs) throws SQLException {
+		return new Compte(rs.getString("login"),rs.getString("password"),rs.getString("typeCompte"));
+	}
 
 	public void insert(Compte obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("insert into compte (login, password,typeCompte) values (?,?,?)",Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, obj.getLogin());
+			ps.setString(2, obj.getPassword());
+			ps.setString(3, obj.getTypeCompte());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 		
 	}
 
 	public void update(Compte obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("update compte set login=?,password=?,typeCompte=? where id_patient=?");
+			ps.setString(1, obj.getLogin());
+			ps.setString(2, obj.getPassword());
+			ps.setString(3, obj.getTypeCompte());
+			ps.setInt(4, obj.getId());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 		
 	}
 
 	public void delete(Compte obj) {
-		// TODO Auto-generated method stub
+		deleteByKey(obj.getId());
 		
 	}
 
-	public void deleteByKey(Integer key) {
-		// TODO Auto-generated method stub
+	public void deleteByKey(Integer id) {
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("delete from compte where id=?");
+			ps.setInt(1, id);
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 		
 	}
 
-	public Compte findByKey(Integer key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Compte findByKey(Integer id) {
+		PreparedStatement ps = null;
+		Compte compte = null;
+		try {
+			ps=Context.getContext().getConnection().prepareStatement("select * from compte where id=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				compte = getCompte(rs);
+			}
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
+		return compte;
 	}
 
 	public List<Compte> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Statement st = null;
+		List<Compte> comptes = new ArrayList<Compte>();
+		Compte compte = null;
+		try {
+			st= Context.getContext().getConnection().createStatement();
+			ResultSet rs = st.executeQuery("select * from compte");
+			if (rs.next()) {
+				compte = getCompte(rs);
+				comptes.add(compte);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
+		return comptes;
 	}
 
 }
--- src/main/java/hopital/dao/DaoPatientImpl.java
+++ src/main/java/hopital/dao/DaoPatientImpl.java
@@ -1,39 +1,108 @@
 package hopital.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 import hopital.model.Patient;
 
 public class DaoPatientImpl implements DaoPatient {
+	
+	private Patient getPatient(ResultSet rs) throws SQLException {
+		return new Patient(rs.getString("nom"),rs.getString("prenom"));
+	}
 
 	public void insert(Patient obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("insert into patient (nom, prenom) values (?,?)",Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, obj.getNom());
+			ps.setString(2, obj.getPrenom());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 		
 	}
 
 	public void update(Patient obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("update patient set nom=?,prenom=? where id_patient=?");
+			ps.setString(1, obj.getNom());
+			ps.setString(2, obj.getPrenom());
+			ps.setInt(3, obj.getId_patient());
+			ps.executeUpdate();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 		
 	}
 
 	public void delete(Patient obj) {
-		// TODO Auto-generated method stub
-		
+		deleteByKey(obj.getId_patient());
 	}
 
-	public void deleteByKey(Integer key) {
-		// TODO Auto-generated method stub
-		
+	public void deleteByKey(Integer id) {
+		PreparedStatement ps = null;
+		try {
+			ps = Context.getContext().getConnection().prepareStatement("delete from patient where id=?");
+			ps.setInt(1, id);
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
 	}
 
-	public Patient findByKey(Integer key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Patient findByKey(Integer id) {
+		PreparedStatement ps = null;
+		Patient patient = null;
+		try {
+			ps=Context.getContext().getConnection().prepareStatement("select * from patient where id=?");
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				patient = getPatient(rs);
+			}
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
+		return patient;
 	}
 
 	public List<Patient> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Statement st = null;
+		List<Patient> patients = new ArrayList<Patient>();
+		Patient patient = null;
+		try {
+			st= Context.getContext().getConnection().createStatement();
+			ResultSet rs = st.executeQuery("select * from patient");
+			if (rs.next()) {
+				patient = getPatient(rs);
+				patients.add(patient);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Context.destroy();
+		}
+		return patients;
 	}
 
 
